03b84a63da33658263b22936c9d4b661cb2cd4f5
Diogo Borges Corso <diogobc0702@gmail.com>
2026-05-20 14:22:50 -0300

    agenda/sessions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 DATABASE = 'estudio.db'
@@ -8,6 +9,7 @@ DATABASE = 'estudio.db'
 def init_db():
     conn = sqlite3.connect(DATABASE)
     cursor = conn.cursor()
+    
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS estoque (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,6 +18,18 @@ def init_db():
             quantidade INTEGER NOT NULL
         )
     ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS agendamentos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome_cliente TEXT NOT NULL,
+            telefone TEXT,
+            data_hora TEXT NOT NULL,
+            descricao_tatuagem TEXT,
+            status TEXT DEFAULT 'Pendente'
+        )
+    ''')
+    
     conn.commit()
     conn.close()
 
@@ -66,6 +80,53 @@ def deletar_material(id):
     conn.commit()
     conn.close()
     return redirect(url_for('index'))
+
+
+@app.route('/agenda')
+def agenda():
+    conn = sqlite3.connect(DATABASE)
+    
+    agora = datetime.now().strftime('%Y-%m-%dT%H:%M')
+    
+    conn.execute("UPDATE agendamentos SET status = 'Concluído' WHERE data_hora < ? AND status = 'Pendente'", (agora,))
+    conn.commit()
+
+    conn.row_factory = sqlite3.Row
+    agendamentos = conn.execute("SELECT * FROM agendamentos ORDER BY status DESC, data_hora ASC").fetchall()
+    conn.close()
+    
+    return render_template('agenda.html', agendamentos=agendamentos)
+
+@app.route('/novo_agendamento', methods=['POST'])
+def novo_agendamento():
+    cliente = request.form['cliente']
+    telefone = request.form['telefone']
+    data_hora = request.form['data_hora']
+    descricao = request.form['descricao']
+    
+    conn = sqlite3.connect(DATABASE)
+    conn.execute('INSERT INTO agendamentos (nome_cliente, telefone, data_hora, descricao_tatuagem) VALUES (?, ?, ?, ?)', 
+                 (cliente, telefone, data_hora, descricao))
+    conn.commit()
+    conn.close()
+    
+    return redirect(url_for('agenda'))
+
+@app.route('/mudar_status_agenda/<int:id>/<status>')
+def mudar_status_agenda(id, status):
+    conn = sqlite3.connect(DATABASE)
+    conn.execute('UPDATE agendamentos SET status = ? WHERE id = ?', (status, id))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('agenda'))
+
+@app.route('/deletar_agendamento/<int:id>')
+def deletar_agendamento(id):
+    conn = sqlite3.connect(DATABASE)
+    conn.execute('DELETE FROM agendamentos WHERE id = ?', (id))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('agenda'))
 
 if __name__ == '__main__':
     init_db()
